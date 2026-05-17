2ae50a6178506c2f4fcb7b75acd80361de0c20e1
Smart Ocquaye <smartocquaye12345@gmail.com>
2026-05-17 22:27:07 -0100

    initcommit

--- src/RequestController.java
+++ src/RequestController.java
@@ -1,12 +1,23 @@
+import java.lang.classfile.constantpool.DoubleEntry;
+import java.util.ArrayList;
+
 public class RequestController {
     public void ServerResponse(){
-
+        ArrayList<Double> parsedNumbers = new ArrayList<>();
     }
-    public void ProcessRequest(ClientRequest request){
+    public void ProcessRequest(ClientRequest request, ArrayList<Double> parsedNumbers){
         //Check endpoint which is kinda like the input
         String userInput = request.endpoint.trim().toLowerCase();
         if(userInput.equals("api") || userInput.equals("analytics") || userInput.equals("calculate")){
             System.out.println("valid");
+            String[] myToken = request.payload.split(",");
+            for(String token:myToken){
+                token.trim();
+                double newToken = Double.parseDouble(token);
+                parsedNumbers.add(newToken);
+            }
+            //we have our parsedNumbers at this point
+
         }
         else{
             ServerResponse toUser = new ServerResponse();
