282678ce0baeb6139fd3d24d3786f644de01e6de
Smart Ocquaye <smartocquaye12345@gmail.com>
2026-05-17 21:49:20 -0100

    initcommit

--- src/RequestController.java
+++ src/RequestController.java
@@ -4,6 +4,5 @@ public class RequestController {
     }
     public void ProcessRequest(ClientRequest request){
         //Check endpoint which is kinda like the input
-        System.out.println(request.endpoint);
     }
 }
