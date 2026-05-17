83c60027b84cfce98ec2aeaeeac974ddc5ae24f0
Smart Ocquaye <smartocquaye12345@gmail.com>
2026-05-17 22:01:19 -0100

    initcommit

--- src/Endpoint.java
+++ src/Endpoint.java
@@ -1,5 +1,5 @@
 public enum Endpoint {
     API,
     ANALYTICS,
-    CALCULATE
+    CALCUlATE
 }
--- src/RequestController.java
+++ src/RequestController.java
@@ -4,5 +4,13 @@ public class RequestController {
     }
     public void ProcessRequest(ClientRequest request){
         //Check endpoint which is kinda like the input
+        String userInput = request.endpoint.trim().toLowerCase();
+        if(userInput.equals("api") || userInput.equals("analytics") || userInput.equals("calculate")){
+            System.out.println("valid");
+        }
+        else{
+            ServerResponse toUser = new ServerResponse();
+            System.out.println(toUser.statusCode);
+        }
     }
 }
