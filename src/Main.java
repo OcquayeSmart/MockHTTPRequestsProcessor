0d08e7c410b0a871e6f73aed22a3000bacea18cb
Smart Ocquaye <smartocquaye12345@gmail.com>
2026-05-18 16:08:30 -0100

    Fixed ExceptionOutOfBounds error

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args){
         ClientRequest request =  new ClientRequest("API", "3.0, -4.0, 12.5, -9.9");
         RequestController controller = new RequestController();
         ServerResponse requests = controller.ProcessRequest(request);
+
     }
 }
--- src/RequestController.java
+++ src/RequestController.java
@@ -2,22 +2,21 @@ import java.util.ArrayList;
 
 public class RequestController {
     ArrayList<Double> parsedNumbers = new ArrayList<>();
-    static ServerResponse errorCode1 = new ServerResponse(400, "Bad request: Invalid Endpoint", "No processing took place");
-    static ServerResponse successCode = new ServerResponse(200, "OK", "Metrics Summary: Max Abs = %f | Ceil Max = %d | Hypotenuse = %f".formatted(RequestController.maxValue, RequestController.ceilMaxValue, RequestController.hypotenuse));
-    static double maxValue;
+    static double maxValue = 0;
+    double nextValue;
     static int ceilMaxValue;
     static int counter = 0;
-    static double firstValue;
-    static double secondValue;
-    static double hypotenuse;
+    static double firstValue, secondValue, hypotenuse, currentValue;
     static String summary;
     static String userInput;
+    static ServerResponse errorCode1 = new ServerResponse(400, "Bad request: Invalid Endpoint", "No processing took place");
+    static ServerResponse successCode = new ServerResponse(200, "OK", "Metrics Summary: Max Abs = %f | Ceil Max = %d | Hypotenuse = %f".formatted(maxValue, ceilMaxValue, hypotenuse));
+
     public ServerResponse ProcessRequest(ClientRequest request){
         //Check endpoint which is kinda like the input
         userInput = request.getEndpoint().trim().toLowerCase();
         //endpoint validation
         if(userInput.equals(Endpoint.API.name().toLowerCase()) || userInput.equals(Endpoint.ANALYTICS.name().toLowerCase()) || userInput.equals(Endpoint.CALCUlATE.name().toLowerCase())){
-            System.out.println("valid");
             String[] myToken = request.getPayload().split(",");
             for(String token:myToken){
                 double newToken = Double.parseDouble(token.trim());
@@ -26,22 +25,24 @@ public class RequestController {
                 //this is the new payload, don't know what to do with it yet
             }
             //hypotenuse approximation
-            for(int i = 0; i < parsedNumbers.size(); i++){
-                if(counter>=2){
-                    firstValue = parsedNumbers.get(i);
-                    secondValue = parsedNumbers.get(i+1);
-                    hypotenuse = Math.sqrt(Math.pow(firstValue, 2) + Math.pow(secondValue, 2));
-                }
-                else{
-                    hypotenuse = 0.0;
-                }
+            if(parsedNumbers.size() > 1){
+                firstValue = parsedNumbers.get(0);
+                secondValue = parsedNumbers.get(1);
+                hypotenuse = Math.sqrt(Math.pow(firstValue, 2) + Math.pow(secondValue, 2));
+            }
+            else if(parsedNumbers.isEmpty()){
+                hypotenuse = 0.0;
             }
             //we have our parsedNumbers at this point
-            for(int i = 0; i < parsedNumbers.size(); i++){
-                double currentValue = parsedNumbers.get(i);
+            for(int i = 0; i < parsedNumbers.size() - 1; i++){
+                currentValue = parsedNumbers.get(i);
+                nextValue = parsedNumbers.get(i+1);
                 maxValue = currentValue;
-                if(parsedNumbers.get(i+1) > currentValue){
-                    maxValue = parsedNumbers.get(i+1);
+                if(currentValue < nextValue){
+                    maxValue = nextValue;
+                }
+                else if(nextValue < currentValue){
+                    maxValue = currentValue;
                 }
             }
             //ceiling of the maximum
