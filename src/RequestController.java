2085081811f5bf82d2a62479165d4ea4659bab4e
Smart Ocquaye <smartocquaye12345@gmail.com>
2026-05-17 23:07:18 -0100

    featureUpdate

--- src/RequestController.java
+++ src/RequestController.java
@@ -1,9 +1,11 @@
-import java.lang.classfile.constantpool.DoubleEntry;
 import java.util.ArrayList;
 
 public class RequestController {
+    ArrayList<Double> parsedNumbers = new ArrayList<>();
+    ServerResponse toUser = new ServerResponse();
     public void ServerResponse(){
-        ArrayList<Double> parsedNumbers = new ArrayList<>();
+
+
     }
     public void ProcessRequest(ClientRequest request, ArrayList<Double> parsedNumbers){
         //Check endpoint which is kinda like the input
@@ -12,19 +14,24 @@ public class RequestController {
             System.out.println("valid");
             String[] myToken = request.payload.split(",");
             for(String token:myToken){
-                token.trim();
-                double newToken = Double.parseDouble(token);
-                parsedNumbers.add(newToken);
+                double newToken = Double.parseDouble(token.trim());
+                parsedNumbers.add(Math.abs(newToken));
+                //this is the new payload, don't know what to do with it yet
             }
             //we have our parsedNumbers at this point
+            System.out.println(Math.max(parsedNumbers));
 
         }
+
         else{
-            ServerResponse toUser = new ServerResponse();
+
             toUser.statusCode = 400;
             System.out.println("Bad Request: Invalid Endpoint");
             //will later remove
             System.out.println("empty body//");
         }
+
+
+
     }
 }
